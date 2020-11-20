9dc367c872315f6d740f977793095373b47ac182
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-19 20:15:48 -0800

    b

--- do_experiment.py
+++ do_experiment.py
@@ -6,9 +6,12 @@ import load_vectors
 
 if __name__ == "__main__":
     embeddings = load_vectors.sequential_embedding.load("../sgns", range(1980, 2000, 10))
-    for year, embed in embeddings.items():
-        print(embed.vocab[0:10])
+    
     time_sims = embeddings.get_time_sims("she", "homemaker")
     print("Similarity between gay and lesbian from 1980 to 1990:")
     for year, sim in time_sims.items():
         print(sim)
+
+def get_random_words(path, year, num_words):
+    embedding = load_vectors.load_vector(path + "/" + str(year))
+    print(embedding.dimension)
