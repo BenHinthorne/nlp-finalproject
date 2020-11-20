fe2645a96832061d7f0ed63df0171e54ececf5e1
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-19 19:52:53 -0800

    haha

--- do_experiment.py
+++ do_experiment.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     embeddings = load_vectors.sequential_embedding.load("../sgns", range(1980, 2000, 10))
     time_sims = embeddings.get_time_sims("lesibian", "gay")
     print("Similarity between gay and lesbian from 1980 to 1990:")
-    for year, sim in time_sims.iteritems():
+    for year, sim in time_sims.items():
         print(year + sim)
