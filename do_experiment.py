7503708c5b486f4011852b803c59836ef5f028f5
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-19 22:27:50 -0800

    boo

--- do_experiment.py
+++ do_experiment.py
@@ -39,8 +39,18 @@ if __name__ == "__main__":
     time_sims = embeddings.get_time_sims("she", "homemaker")
     for year, sim in time_sims.items():
         print("{}: {}".format(year,sim))
+    
+    time_sims = embeddings.get_time_sims("he", "homemaker")
+    for year, sim in time_sims.items():
+        print("{}: {}".format(year,sim))
+
+    
 
     time_sims = embeddings.get_time_sims("he", "skipper")
+    for year, sim in time_sims.items():
+        print("{}: {}".format(year,sim))
+
+    time_sims = embeddings.get_time_sims("she", "skipper")
     for year, sim in time_sims.items():
         print("{}: {}".format(year,sim))
 
