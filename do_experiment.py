04a7bcc16f4749acadf89d029753e90d7545cf23
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-19 20:31:06 -0800

    beee

--- do_experiment.py
+++ do_experiment.py
@@ -1,12 +1,23 @@
 import load_vectors
+import random.choice()
+
+
 
 ## Pick some random distribution of words 
 ## Compute Cosine Similarity of for this random distribution of words over different years
 
 def get_random_words(path, year):
     embedding = load_vectors.word_embedding.load_vector(path + "/" + str(year))
-    print(embedding.vocab)
-    print(len(embedding.vocab))
+    random_sims = []
+    for i in range(0,100):
+        sim = embedding.similarity(random.choice(embeddings.vocab), random.choice(embeddings.vocab))
+        if sim < 1:
+            random_sims.append(sim)
+    print(random_sims)
+
+
+    #print(embedding.vocab)
+    #print(len(embedding.vocab))
 
 if __name__ == "__main__":
     embeddings = load_vectors.sequential_embedding.load("../sgns", range(1980, 2000, 10))
