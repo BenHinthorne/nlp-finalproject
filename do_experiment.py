e0c3896512eea34ed04971e2e53a7c0d9ba38bb2
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-19 20:18:02 -0800

    woo

--- do_experiment.py
+++ do_experiment.py
@@ -3,15 +3,16 @@ import load_vectors
 ## Pick some random distribution of words 
 ## Compute Cosine Similarity of for this random distribution of words over different years
 
+def get_random_words(path, year, num_words):
+    embedding = load_vectors.load_vector(path + "/" + str(year))
+    print(embedding.dimension)
 
 if __name__ == "__main__":
     embeddings = load_vectors.sequential_embedding.load("../sgns", range(1980, 2000, 10))
-    
+    get_random_words("../sgns", 1980)
     time_sims = embeddings.get_time_sims("she", "homemaker")
     print("Similarity between gay and lesbian from 1980 to 1990:")
     for year, sim in time_sims.items():
         print(sim)
 
-def get_random_words(path, year, num_words):
-    embedding = load_vectors.load_vector(path + "/" + str(year))
-    print(embedding.dimension)
+
