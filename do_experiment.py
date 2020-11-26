8b2cb49c62ff1124611c1cec1da5fcdf2303e73f
BenHinthorne <ben.hinthorne@gmail.com>
2020-11-25 16:10:33 -0800

    sbrn

--- do_experiment.py
+++ do_experiment.py
@@ -4,6 +4,7 @@ import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
+import seaborn as sns
 
 
 def run_experiment(occupations, start_year, end_year):
@@ -103,6 +104,9 @@ def plot_difference(df, label_1, label_2):
     df.trendline.sort_index(ascending=False).plot(label=label, ax=ax)
     plt.gca().invert_xaxis()
     plt.savefig("uhhh.png")
+
+    sns.regplot(df['year'], df['diff'])
+    sns.savefig("sb.png")
 
     
 
